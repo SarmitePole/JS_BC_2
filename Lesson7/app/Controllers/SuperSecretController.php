cbff7c65b5470dafd8336d9a75d9e4642b2cb506
SarmitePole <sa.pole@inbox.lv>
2020-03-02 15:46:22 +0200

    Added pasword ecryption

--- Lesson7/app/Controllers/SuperSecretController.php
+++ Lesson7/app/Controllers/SuperSecretController.php
@@ -7,6 +7,7 @@ class SuperSecretController
        if ($_SESSION['loggedIn'] != true) {
            header('Location: ?page=login');
            exit;
+           //exit aptur lapas izpildi, tja viņa nebūs, tad neatļautā lapa paliks atvērta, pat, ja sesijas kukijs būs deletēts
        }
 
        return BaseView::generate('SuperSecret', 'index');
