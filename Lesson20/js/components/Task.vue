fea75b6e96c42dc9685d534b78d3c50b63f4158f
student <sa.pole@inbox.lv>
2020-03-05 21:21:09 +0200

    Lesson20

---
+++ Lesson20/js/components/Task.vue
@@ -0,0 +1,25 @@
+<template>
+    <li>
+        <del v-if="taskk.isCompleted">{{ taskk.title }}</del>
+        <sapn v-else>{{ tasks.title}}</span>
+    </li>
+    
+</template>
+
+
+<style>
+
+
+</style>
+
+
+
+<script>
+export default {
+    props: {
+        taskk: {
+            required: true,
+        }
+    }    
+}
+</script>
