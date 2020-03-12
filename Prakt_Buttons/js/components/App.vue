a166ec4dcde97dcfa0924fb9e0c30dcee45543b8
student <sa.pole@inbox.lv>
2020-03-12 21:00:30 +0200

    ops

---
+++ Prakt_Buttons/js/components/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container m-3">
+        <div>
+
+            <button
+                :class="getBtnClass('Foo')"
+                @click="currentValue = 'Foo'"
+            >Foo</button>
+
+            <button
+                :class="getBtnClass('Bar')" 
+                @click="currentValue = 'Bar'"
+            >Bar</button>
+
+            <button
+                :class="getBtnClass('Baz')"
+                @click="currentValue = 'Baz'"
+            >Baz</button>
+
+            <button
+                :class="getBtnClass('dd')" 
+                @click="currentValue = 'dd'"
+            >dd</button>
+
+                  
+            <button
+                :class="getBtnClass('Foo')"
+                @click="currentValue = 'Foo'"
+            >Foo</button>
+            
+        </div>
+    </div>   
+</template>
+
+
+<style>
+.red-btn {
+    background: red;
+}
+
+
+</style>
+
+
+
+<script>
+
+export default {
+    data() {
+        return {
+            currentValue: 'Foo'
+        } 
+    },
+    
+    methods: {       
+        getBtnClass(btnName) {
+        if (btnName == this.currentValue) {
+            return 'red-btn';
+        } else {
+            return '';
+
+        }
+           
+    } 
+}
+}
+
+
+</script>
